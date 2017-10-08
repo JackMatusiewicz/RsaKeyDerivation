9cb256661e9a671cb41bd5ac4436a802c61be5b8
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2017-10-08 14:12:49 +0100

    Reording prime generation code

--- RsaKeyDerivation/Prime.fs
+++ RsaKeyDerivation/Prime.fs
@@ -6,55 +6,55 @@ module Prime =
 
     type WitnessCheckResult = Composite | Continue
 
-    let isPrime (k : int) =
+    let private calculateComposite (n : bigint) : (int * bigint) =
+        let rec calc ((s,d) : int * bigint) : (int * bigint) =
+            if d.IsEven then
+                calc (s + 1, d / (bigint 2))
+            else
+                (s,d)
+        calc (0, n)
+
+    let millerRabinTest (k : int) (n : bigint) : State<Csprng, bool> =
+        let rec witnessCheck (x : bigint) (n : bigint) (r : int) : WitnessCheckResult =
+            match r with
+            | 0 -> Composite
+            | _ ->
+                let newX = BigInteger.ModPow(x, (bigint 2), n)
+                if newX = (bigint 1) then
+                    Composite
+                else if newX = (n - (bigint 1)) then
+                    Continue
+                else
+                    witnessCheck newX n (r - 1)
+
+        let rec attempts (k : int) : State<Csprng, bool> = state {
+            match k with
+            | 0 -> return true
+            | _ ->
+                let (s,d) = calculateComposite (n - bigint 1)
+                let!  a = Csprng.range (bigint 2) (n - bigint 2)
+                let x = BigInteger.ModPow(a, d, n)
+                if (x = bigint 1 || x = (n - bigint 1)) then
+                    return! attempts (k - 1)
+                else
+                    match witnessCheck x n (s - 1) with
+                    | Composite -> return false
+                    | Continue -> return! attempts (k - 1)
+        }
+        attempts k
+
+    let isPrime (checks : int) =
         let remainderIsZero num denom = BigInteger.Remainder(num, denom) = (bigint 0)
         let smallPrimes = [2;3;5;7;11] |> List.map bigint
         let smallPrimeTest = fun v -> 
             let isMultipleOfPrimes = List.map (remainderIsZero v) smallPrimes
             not <| List.fold (||) false isMultipleOfPrimes
 
-        let calculateComposite (n : bigint) : (int * bigint) =
-                let rec calc ((s,d) : int * bigint) : (int * bigint) =
-                    if d.IsEven then
-                        calc (s + 1, d / (bigint 2))
-                    else
-                        (s,d)
-                calc (0, n)
-
-        let millerRabin (k : int) (n : bigint) : State<Csprng, bool> =
-            let rec witnessCheck (x : bigint) (n : bigint) (r : int) : WitnessCheckResult =
-                match r with
-                | 0 -> Composite
-                | _ ->
-                    let newX = BigInteger.ModPow(x, (bigint 2), n)
-                    if newX = (bigint 1) then
-                        Composite
-                    else if newX = (n - (bigint 1)) then
-                        Continue
-                    else
-                        witnessCheck newX n (r - 1)
-            
-            let rec attempts (k : int) : State<Csprng, bool> = state {
-                match k with
-                | 0 -> return true
-                | _ ->
-                    let (s,d) = calculateComposite (n - bigint 1)
-                    let!  a = Csprng.range (bigint 2) (n - bigint 2)
-                    let x = BigInteger.ModPow(a, d, n)
-                    if (x = bigint 1 || x = (n - bigint 1)) then
-                        return! attempts (k - 1)
-                    else
-                        match witnessCheck x n (s - 1) with
-                        | Composite -> return false
-                        | Continue -> return! attempts (k - 1)
-            }
-            attempts k
-
         fun n ->
             if List.contains n smallPrimes then
                 lift true
             else
-                (&&) <!> (lift <| (smallPrimeTest n)) <*> (millerRabin 5 n)
+                (&&) <!> (lift <| (smallPrimeTest n)) <*> (millerRabinTest checks n)
 
     let findPrime (k : int) (start : bigint) : State<Csprng, bigint> =
         let rec findNextPrime (current : bigint) : State<Csprng, bigint> = state {
