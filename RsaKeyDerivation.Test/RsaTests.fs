1d3dc4709440ca8b40407e0d36c9ce35b41bfdc0
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-04 12:16:06 +0000

    Adding test to prove that key derivation is deterministic based on CSPRNG

--- RsaKeyDerivation.Test/RsaTests.fs
+++ RsaKeyDerivation.Test/RsaTests.fs
@@ -7,6 +7,7 @@ module RsaTests =
     open System.Security.Cryptography
     open System.Text
     open System
+    open System.Linq
 
     let private encrypt (data : byte[]) (key : RSAParameters) =
         use rsa = new RSACryptoServiceProvider()
@@ -30,3 +31,20 @@ module RsaTests =
         let decString = byteConverter.GetString decData
         Assert.That(decString, Is.EqualTo("hello, world"))
         ()
+
+    [<Test>]
+    [<Repeat(10)>]
+    let ``Using the same csprng gives you exactly the same Rsa key``() =
+        let keysAreIdentical (a : RSAParameters) (b : RSAParameters) =
+            Assert.That(a.D.SequenceEqual(b.D), Is.True)
+            Assert.That(a.DP.SequenceEqual(b.DP), Is.True)
+            Assert.That(a.DQ.SequenceEqual(b.DQ), Is.True)
+            Assert.That(a.Exponent.SequenceEqual(b.Exponent), Is.True)
+            Assert.That(a.InverseQ.SequenceEqual(b.InverseQ), Is.True)
+            Assert.That(a.Modulus.SequenceEqual(b.Modulus), Is.True)
+            Assert.That(a.P.SequenceEqual(b.P), Is.True)
+            Assert.That(a.Q.SequenceEqual(b.Q), Is.True)
+        let csprng = Csprng.create ()
+        let key,_ = runState (Rsa.createKey 1024 8) csprng
+        let key2,_ = runState (Rsa.createKey 1024 8) csprng
+        keysAreIdentical key key2
